4111bda38e6e08d39ec48adebd5433a73c96c286
Chris Kwan <chriswkwan@gmail.com>
2015-09-16 08:21:13 -0400

    Add path drawing on mouse move

--- invisible-ink.js
+++ invisible-ink.js
@@ -1,17 +1,38 @@
 (function () {
+	var pathToDraw = [];
+	var drawingArea = document.getElementById("drawingArea");
+	var context = drawingArea.getContext("2d");
+	context.fillStyle = "blue";
+
 	initialize();
 
 	function initialize() {
-		setUpUI();
+		setUpCanvasEvents();
 	}
 
-	function setUpUI(){
-		var drawingArea = document.getElementById("drawingArea");
-		drawingArea.onclick = function (event) {
-			var context = drawingArea.getContext("2d");
-			context.fillStyle = "blue";
+	function setUpCanvasEvents(){
+		drawingArea.onmousedown = function (event) {
+			// Reset path
+			pathToDraw = [];
+		}
+
+		drawingArea.onmousemove = function (event) {
 			var rect = drawingArea.getBoundingClientRect();
-			context.fillRect(event.clientX - rect.left, event.clientY - rect.top, 50, 50);
+			pathToDraw.push({
+				x: event.clientX - rect.left,
+				y: event.clientY - rect.top
+			})
+		}
+
+		drawingArea.onmouseup = function (event) {
+			drawPath();
+		}
+	}
+
+	function drawPath() {
+		for (var i=0; i<pathToDraw.length; i++) {
+			var point = pathToDraw[i];
+			context.fillRect(point.x, point.y, 5, 5);
 		}
 	}
 })();
